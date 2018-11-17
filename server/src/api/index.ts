b858ac6d795253013cd3995ffcde62c689c04fbc
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-11-17 10:59:52 +0100

    Connexion à MongoDB

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,10 +1,12 @@
 import endpoints from './endpoints/index';
+import db from '../db/index';
 import { success } from '../utils/logger';
 
-export default () => {
-    success('API chargée');
+export default async () => {
+    await db();
     // On boucle sur les endpoints exportés
     endpoints.forEach((loadEndpoint: () => void) => {
         loadEndpoint();
     });
+    success('API chargée');
 };
