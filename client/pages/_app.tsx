21147962b53c925959afc3d1029fbf78eeadfaf3
Maxime Bertonnier <m.bertonnier@gmail.com>
2019-01-23 11:37:15 +0100

    Rendu du header, du composant actif et du ScanButton

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import Header from '../components/Header';
+import ScanButton from '../components/ScanButton';
 import '../styles/main.scss';
 
 export default class MyApp extends App {
+
+    constructor (props: any) {
+        super(props);
+        // L'index de la vue active est stockée dans le state au plus haut niveau de l'application
+        this.state = { activeViewIndex: 0 };
+        this.onChangeActiveView = this.onChangeActiveView.bind(this);
+    }
 
     // Configuration spécifique à next récupérée sur la documentation.
     // https://github.com/zeit/next.js/#custom-app
@@ -14,10 +23,23 @@ export default class MyApp extends App {
         return { pageProps };
     }
 
+    onChangeActiveView (index: number) {
+        this.setState({ activeViewIndex: index });
+    }
+
     render () {
+        const { Component, pageProps, router } = this.props;
         return (
             <Container>
-                <p>PWA !</p>
+                <Header
+                  activeViewIndex={this.state.activeViewIndex}
+                  onChangeActiveView={this.onChangeActiveView}
+                  route={router.route}
+                />
+                <div className={'App--content-container'}>
+                    <Component {...pageProps} activeViewIndex={this.state.activeViewIndex} />
+                </div>
+                <ScanButton route={router.route}/>
             </Container>
         );
     }
