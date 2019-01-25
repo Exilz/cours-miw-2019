273cc0716c7b4382ef5846d74864a5421115437d
Maxime Bertonnier <m.bertonnier@gmail.com>
2019-01-25 17:49:06 +0100

    Ajout du lien vers le manifest et les icônes dans le head

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -10,9 +10,18 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta name='theme-color' content='#14b45a' />
                     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                    <link rel='manifest' href='/static/manifest.json' />
                     <link rel='stylesheet' href='/_next/static/style.css' />
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous"></link>
                     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" />
+
+                    <link rel='icon' type='image/x-icon' href='/static/images/favicon/32.png' />
+                    <link rel='shortcut icon' type='image/x-icon' href='/static/images/favicon/32.png' />
+
+                    <link rel='icon' type='image/png' sizes='32x32' href='/static/images/favicon/32.png' />
+                    <link rel='icon' type='image/png' sizes='96x96' href='/static/images/favicon/96.png' />
+                    <link rel='icon' type='image/png' sizes='192x192' href='/static/images/favicon/192.png' />
+
                 </Head>
                 <body>
                     <Main />
